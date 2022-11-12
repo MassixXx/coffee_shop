01ccf7aa4301c7557bbfbd9569c9bba8350e8699
MassixXx <km_abboud@esi.dz>
2022-11-12 17:36:57 +0100

    update environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-e-0805z7.us', // the auth0 domain prefix
+    audience: 'dev', // the audience set for the auth0 app
+    clientId: 'jpRbXkci0EtRgHyQKcjTxRhHN8hx0WpA', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
